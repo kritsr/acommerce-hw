1b57e4a9ae3b58a81a8c69ba148f8b5da4f75e93
Krit Sriyanuwatkul <krit2531@hotmail.com>
2019-07-02 02:46:47 +0700

    Updates

---
+++ precache-manifest.30f953b741609e92b607f23a5cb6f0d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3e50cbb35980fbb0d8aa02fade2d59",
+    "url": "/acommerce-hw/index.html"
+  },
+  {
+    "revision": "ea191adbfd094e6d4b3d",
+    "url": "/acommerce-hw/static/css/main.3aa049fd.chunk.css"
+  },
+  {
+    "revision": "69d79f368d6248d8a1c7",
+    "url": "/acommerce-hw/static/js/2.0bf725f1.chunk.js"
+  },
+  {
+    "revision": "ea191adbfd094e6d4b3d",
+    "url": "/acommerce-hw/static/js/main.c018188d.chunk.js"
+  },
+  {
+    "revision": "8c14c4623891bbd8c194",
+    "url": "/acommerce-hw/static/js/runtime~main.36f25a5c.js"
+  }
+]);
